3816e0be1649c1a837eb1605798654542c314fee
jeeeanlefchak <jeeeanlefchak>
2022-07-26 13:55:29 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'interface-netflix',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
